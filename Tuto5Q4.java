685dc4435bf7c57ef2540bc6422f6221ecd0bea3
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-08-03 23:48:44 +0530

    another example for ternary operators

---
+++ Tuto5Q4.java
@@ -0,0 +1,18 @@
+class Tuto5Q4{
+	
+	public static void main(String args[]){
+		
+		double service = 4;
+		double gratitude;
+		
+		gratitude = (service>20)?((service>5)?(50000):(service*2500+50000)):(service*2500+100000);
+		
+		System.out.println(gratitude);
+		
+		
+		
+		
+	}
+	
+	
+}
